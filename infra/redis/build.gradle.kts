f3ff1679b64e051e411cf7aa73170f7e1fa34c7c
noah <noah@techtaka.com>
2024-10-16 18:27:11 +0900

    redis setting

--- infra/redis/build.gradle.kts
+++ infra/redis/build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     id("com.jinuk.toy.lib")
-    id("com.jinuk.toy.spring-boot-jpa")
     id("java-test-fixtures")
 }
 
 dependencies {
-    runtimeOnly(Dependencies.Mysql.CONNECTOR)
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     testFixturesImplementation(Dependencies.SpringBoot.SPRING_BOOT_STARTER_TEST)
     testFixturesImplementation(Dependencies.TestContainers.JUNIT_JUPITER)
-    testFixturesImplementation(Dependencies.TestContainers.MYSQL)
 }
